51e962145a01bff96c4ec010748710c09fea48e4
andreecampos <orlandoandree1998@gmail.com>
2022-03-01 22:18:58 +0100

    Heroku create

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ dotenv.config();
 /*1.4  i terminalen 
 git init .
 ls
-heroku create
+heroku create, sen click på första länken
+git status, git add . , git status, git status,  git add Procfile, 
+git commit -m 'skriv  något', git push herroku master
 */
+//1.5 skapar Procfile
+//1.6 efter att vi har commitat så finns ett länk och clickar där
 const app = express()
 //1.1 heroku bestämmer vår port process.env.PORT 
 //1.2 npm install dotenv för att  öppna  server PORT=3000
